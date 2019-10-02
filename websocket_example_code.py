2b1b7d1863bedcae2df3cb107bc6b9b5a0dcac8d
dlwilson355 <dlwilson355@gmail.com>
2019-10-02 12:01:14 -0400

    Code improvements.

--- websocket_example_code.py
+++ websocket_example_code.py
@@ -11,19 +11,15 @@ Created on Tue Oct  1 08:32:06 2019
 import websocket
 import sqlite3
 import json
-print(websocket.__file__)
 import requests
-from botsettings import API_TOKEN
 try:
     import thread
 except ImportError:
     import _thread as thread
-import time
-sqlite3 
+from botsettings import API_TOKEN
+
 action = ""
-from sqlite3 import Error
- 
- 
+
 #def create_connection(db_file):
 #    """ create a database connection to a SQLite database """
 #    conn = None
@@ -48,94 +44,125 @@ from sqlite3 import Error
 #    finally:
 #        if conn:
 #            conn.close()
-            
+
+
 def on_message(ws, message):
     category_actions = ['time', 'pizza', 'greet', 'weather', 'joke']
     global action
+
+    # these print statements are only temporary for debugging
+    print("Message Payload...")
     print(message)
-    if "awknowledge" in message:
-        to_send = {"id": 1, "type": "message", "channel": "CNPJBJZ29", "text": "Awknowledged!"}
-        to_send = json.dumps(to_send)
-        ws.send(to_send)
-    if "training" in message and 'reply_to' not in message:
+    print("Relevant Message Content...")
+    print(get_message_content(message))
+
+    message_content = get_message_content(message)
+
+    if "acknowledge" in message_content:
+        send_message(ws, "Acknowledged")
+
+    if "training" in message_content:
         action = 'training'
-        to_send = {"id": 1, "type": "message", "channel": "CNPJBJZ29", "text": "OK, I'm ready for training. What NAME should this ACTION be?"}
-        to_send = json.dumps(to_send)
-        ws.send(to_send)
-    if 'done' in message.lower():
+        send_message(ws, "OK, I'm ready for training. What NAME should this ACTION be?")
+
+    if 'done' in message_content:
         action = 'done'
-        to_send = {"id": 1, "type": "message", "channel": "CNPJBJZ29", "text": "OK, I'm finished training"}
-        to_send = json.dumps(to_send)
-        ws.send(to_send)
-    if action in category_actions and '"type":"message"' in message:
+        send_message(ws, "OK, I'm finished training")
+
+    if len(message_content) > 0 and action in category_actions:
         put_in_db(message)
-        to_send = {"id": 1, "type": "message", "channel": "CNPJBJZ29", "text": "OK, I've got it! What else?"}
-        to_send = json.dumps(to_send)
-        ws.send(to_send)
-    if action == 'training' and 'TIME' in message.upper() and 'TRAINING' not in message.upper():
+        send_message(ws, "OK, I've got it! What else?")
+
+    if "time" in message_content and action == 'training':
         action = 'time'
-    if action == 'training' and 'PIZZA' in message.upper():
+
+    if "pizza" in message_content and action == 'training':
         action = 'pizza'
-    if action == 'training' and 'GREET' in message.upper():
+
+    if "greet" in message_content and action == 'training':
         action = 'greet'
-    if action == 'training' and 'WEATHER' in message.upper():
-        action = 'weather'        
-    if action == 'training' and 'JOKE' in message.upper():
-        action = 'joke'        
+
+    if "weather" in message_content and action == 'training':
+        action = 'weather'
+
+    if "joke" in message_content and action == 'training':
+        action = 'joke'
+
+
 def put_in_db(message):
     global action
     conn = sqlite3.connect("name.db")
     c = conn.cursor()
     c.execute("INSERT INTO training_data (txt,action) VALUES (?, ?)", (message, action,))
-    conn.commit() # save (commit) the changes\
-    
+    conn.commit()  # save (commit) the changes\
+
+
 def make_table(database):
     conn = sqlite3.connect("name.db")
     c = conn.cursor() 
     c.execute(database)
     conn.commit()
-    
+
+
+def send_message(ws, text):
+    """Sends a message over the project01 channel with the specified text."""
+
+    dict_payload = {"id": 1, "type": "message", "channel": "CNPJBJZ29", "text": text}
+    json_payload = json.dumps(dict_payload)
+    ws.send(json_payload)
+
+
+def get_message_content(message):
+    """
+    Returns a string containing the text of a message typed by the user.
+
+    The returned message will be converted to lowercase.
+    Any unneeded punctuation will be removed.
+    Returns an empty string if the message was sent by Jarvis.
+    This prevents Jarvis from responding to his own messages.
+    """
+
+    punctuation_to_remove = "~!@#$%^&*()-+=,./<>"
+    json_payload = json.loads(message)
+    if "client_msg_id" in json_payload.keys():
+        text = json_payload["text"].lower()
+        for character in punctuation_to_remove:
+            text = text.replace(character, "")
+        return text
+    return ""
+
+
 def on_error(ws, error):
     print("in on error")
 
     print('The error is:', error)
 
+
 def on_close(ws):
     print("### closed ###")
 
+
 def on_open(ws):
-    pass
-#    def run(*args):
-#        print("in run")
-#        for i in range(3):
-#            message = {"id": 1, "type": "message", "channel": "CNPJBJZ29", "text": "Hello world"}
-#            message = json.dumps(message)
-#            print(type(message))
-#            ws.send(message)
-#        time.sleep(1)
-#        ws.close()
-#        print("thread terminating...")
-#    thread.start_new_thread(run, ())
+    send_message(ws, "Jarvis is online. ;)")
 
 
 if __name__ == "__main__":
-    websocket.enableTrace(True)
-    connect_string = f"http://slack.com/api/rtm.connect"
-    #caaaurl_string = f"wss://slack.com/api/rtm.start?token={API_TOKEN}&pretty=1"
-    #url_string = f"wss://slack.com/api/chat.postMessage?token={API_TOKEN}&channel=project01&text=I'm_Jarvis&as_user=jarvis&pretty=1"
-    url_string = requests.get(connect_string, params = {'token': API_TOKEN}).json()['url']
-    
+    # create an empty database
     sql_create_training_data_table = """ CREATE TABLE IF NOT EXISTS training_data (
-                                         txt text,
-                                         action text
-                                    ); """
+                                             txt text,
+                                             action text
+                                        ); """
     make_table(sql_create_training_data_table)
-    
+
+    # create the connection string
+    connect_string = f"http://slack.com/api/rtm.connect"
+    url_string = requests.get(connect_string, params={'token': API_TOKEN}).json()['url']
+
+    # create the connection
     websocket.enableTrace(True)
     ws = websocket.WebSocketApp(url_string,
                               on_message = on_message,
                               on_error = on_error,
                               on_close = on_close)
     ws.on_open = on_open
-    #on_open(ws)
     ws.run_forever()
