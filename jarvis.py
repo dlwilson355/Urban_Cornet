fe68dc3156dcef4e5543e2644f7bac923da9c65f
Daniel Wilson <dlwilson355@gmail.com>
2019-10-06 01:56:11 -0400

    Database is working.

--- jarvis.py
+++ jarvis.py
@@ -14,158 +14,141 @@ except ImportError:
     import _thread as thread
 from botsettings import API_TOKEN
 
-action = ""
 
-#def create_connection(db_file):
-#    """ create a database connection to a SQLite database """
-#    conn = None
-#    try:
-#        conn = sqlite3.connect(db_file)
-#        print(sqlite3.version)
-#    except Error as e:
-#        print(e)
-#    finally:
-#        if conn:
-#            conn.close()
-#def create_connection():
-#    """ create a database connection to a database that resides
-#        in the memory
-#    """
-#    conn = None;
-#    try:
-#        conn = sqlite3.connect(':memory:')
-#        print(sqlite3.version)
-#    except Error as e:
-#        print(e)
-#    finally:
-#        if conn:
-#            conn.close()
+class Jarvis:
+    def __init__(self):
+        self.action = ""
+        self.connection, self.cursor = self.initialize_database()
+        self.web_socket = self.initialize_slack_connection()
 
+    def initialize_database(self):
+        """This function connects Jarvis to the database."""
 
-def on_message(ws, message):
-    """This function controls the bot's actions in response to a message."""
+        connection = sqlite3.connect("jarvis.db")
+        cursor = connection.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS training_data (txt text, action text)")
+        return connection, cursor
 
-    category_actions = ['time', 'pizza', 'greet', 'weather', 'joke']
-    global action
+    def initialize_slack_connection(self):
+        """This function connects Jarvis to Slack over the RTM API."""
 
-    # these print statements are only temporary for debugging
-    print("Message Payload...")
-    print(message)
-    print("Relevant Message Content...")
-    print(get_message_content(message))
+        # get the url string
+        connect_string = f"http://slack.com/api/rtm.connect"
+        url_string = requests.get(connect_string, params={'token': API_TOKEN}).json()['url']
 
-    message_content = get_message_content(message)
+        # create the connection and set up the websocket
+        websocket.enableTrace(True)
+        ws = websocket.WebSocketApp(url_string,
+                                    on_message=lambda ws, mes: self.on_message(ws, mes),
+                                    on_error=lambda ws, err: self.on_error(ws, err),
+                                    on_close=lambda ws: self.on_close(ws))
+        ws.on_open = lambda ws: self.on_open(ws)
+        ws.run_forever()
 
-    if "acknowledge" in message_content:
-        send_message(ws, "Acknowledged")
-
-    if "training" in message_content:
-        action = 'training'
-        send_message(ws, "OK, I'm ready for training. What NAME should this ACTION be?")
-
-    if 'done' in message_content:
-        action = 'done'
-        send_message(ws, "OK, I'm finished training")
-
-    if len(message_content) > 0 and action in category_actions:
-        put_in_db(message)
-        send_message(ws, "OK, I've got it! What else?")
-
-    if "time" in message_content and action == 'training':
-        action = 'time'
-
-    if "pizza" in message_content and action == 'training':
-        action = 'pizza'
-
-    if "greet" in message_content and action == 'training':
-        action = 'greet'
-
-    if "weather" in message_content and action == 'training':
-        action = 'weather'
-
-    if "joke" in message_content and action == 'training':
-        action = 'joke'
+        return ws
 
 
-def put_in_db(message):
-    global action
-    conn = sqlite3.connect("name.db")
-    c = conn.cursor()
-    c.execute("INSERT INTO training_data (txt,action) VALUES (?, ?)", (message, action,))
-    conn.commit()  # save (commit) the changes\
+    def on_message(self, ws, message):
+        """This function controls the bot's actions in response to a message."""
+
+        category_actions = ['time', 'pizza', 'greet', 'weather', 'joke']
+
+        # these print statements are only temporary for debugging
+        print("Message Payload...")
+        print(message)
+        print("Relevant Message Content...")
+        print(self.get_message_content(message))
+
+        message_content = self.get_message_content(message)
+
+        if "training" in message_content:
+            self.action = 'training'
+            self.send_message(ws, "OK, I'm ready for training. What NAME should this ACTION be?")
+
+        elif 'done' in message_content:
+            self.action = 'done'
+            self.send_message(ws, "OK, I'm finished training")
+
+        elif len(message_content) > 0 and self.action in category_actions:
+            self.add_to_database((message_content, self.action))
+            self.send_message(ws, "OK, I've got it! What else?")
+
+        elif "time" == message_content and self.action == 'training':
+            self.action = 'time'
+            self.send_message(ws, "OK, Let's call this action `TIME`. Now give me some training text!")
+
+        elif "pizza" == message_content and self.action == 'training':
+            self.action = 'pizza'
+            self.send_message(ws, "OK, Let's call this action `PIZZA`. Now give me some training text!")
+
+        elif "greet" == message_content and self.action == 'training':
+            self.action = 'greet'
+            self.send_message(ws, "OK, Let's call this action `GREET`. Now give me some training text!")
+
+        elif "weather" == message_content and self.action == 'training':
+            self.action = 'weather'
+            self.send_message(ws, "OK, Let's call this action `WEATHER`. Now give me some training text!")
+
+        elif "joke" == message_content and self.action == 'training':
+            self.action = 'joke'
+            self.send_message(ws, "OK, Let's call this action `JOKE`. Now give me some training text!")
+
+    def add_to_database(self, entities):
+        """This function adds the passed entities to Jarvis' database."""
+
+        # execute the sql command to modify the table
+        self.cursor.execute("INSERT INTO training_data(txt, action) VALUES(?, ?)", entities)
+
+        # commit the changes
+        self.connection.commit()
 
 
-def make_table(database):
-    conn = sqlite3.connect("name.db")
-    c = conn.cursor() 
-    c.execute(database)
-    conn.commit()
+    def send_message(self, ws, text):
+        """Sends a message over the project01 channel with the specified text."""
+
+        dict_payload = {"id": 1, "type": "message", "channel": "CNPJBJZ29", "text": text}
+        json_payload = json.dumps(dict_payload)
+        ws.send(json_payload)
 
 
-def send_message(ws, text):
-    """Sends a message over the project01 channel with the specified text."""
+    def get_message_content(self, message):
+        """
+        Returns a string containing the text of a message typed by the user.
 
-    dict_payload = {"id": 1, "type": "message", "channel": "CNPJBJZ29", "text": text}
-    json_payload = json.dumps(dict_payload)
-    ws.send(json_payload)
+        The returned message will be converted to lowercase.
+        Any unneeded punctuation will be removed.
+        Returns an empty string if the message was sent by Jarvis.
+        This prevents Jarvis from responding to his own messages.
+        """
 
-
-def get_message_content(message):
-    """
-    Returns a string containing the text of a message typed by the user.
-
-    The returned message will be converted to lowercase.
-    Any unneeded punctuation will be removed.
-    Returns an empty string if the message was sent by Jarvis.
-    This prevents Jarvis from responding to his own messages.
-    """
-
-    punctuation_to_remove = "~!@#$%^&*()-+=,./<>"
-    json_payload = json.loads(message)
-    if "client_msg_id" in json_payload.keys():
-        text = json_payload["text"].lower()
-        for character in punctuation_to_remove:
-            text = text.replace(character, "")
-        return text
-    return ""
+        punctuation_to_remove = "~!@#$%^&*()-+=,./<>"
+        json_payload = json.loads(message)
+        if "client_msg_id" in json_payload.keys():
+            text = json_payload["text"].lower()
+            for character in punctuation_to_remove:
+                text = text.replace(character, "")
+            return text
+        return ""
 
 
-def on_error(ws, error):
-    """Prints an error encountered by the bot."""
+    def on_error(self, ws, error):
+        """Prints an error encountered by the bot."""
 
-    print("in on error")
-    print('The error is:', error)
-
-
-def on_close(ws):
-    print("### closed ###")
+        print("in on error")
+        print('The error is:', error)
 
 
-def on_open(ws):
-    """Sends a message when opening a connection."""
+    def on_close(self, ws):
+        print("### closed ###")
 
-    send_message(ws, "Jarvis is online. ;)")
+
+    def on_open(self, ws):
+        """Sends a message when opening a connection."""
+
+        self.send_message(ws, "Jarvis is online. ;)")
 
 
 if __name__ == "__main__":
     """This code initializes the connection and starts the bot."""
 
-    # create an empty database
-    sql_create_training_data_table = """ CREATE TABLE IF NOT EXISTS training_data (
-                                             txt text,
-                                             action text
-                                        ); """
-    make_table(sql_create_training_data_table)
-
-    # create the connection string
-    connect_string = f"http://slack.com/api/rtm.connect"
-    url_string = requests.get(connect_string, params={'token': API_TOKEN}).json()['url']
-
-    # create the connection
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp(url_string,
-                              on_message = on_message,
-                              on_error = on_error,
-                              on_close = on_close)
-    ws.on_open = on_open
-    ws.run_forever()
+    jarvis = Jarvis()
