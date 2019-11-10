d13d7b470aa7b79dc2b22f09597d7b15365ce7c9
sefergus <52716868+sefergus@users.noreply.github.com>
2019-11-09 19:30:18 -0500

    Works with Jarvis
    lots of little issues, but works.

--- battleship.py
+++ battleship.py
@@ -5,10 +5,11 @@ Created on Thu Nov  7 18:29:45 2019
 
 @author: sarafergus
 """
-letter2num= {'a' : 0, 'b': 1}
+
 boat2length= {'battleship': 4, 'carrier': 5, 'cruiser': 3, 'submarine': 3, 'destroyer':2}
-boat2num = {'battleship': 1, 'carrier': 2, 'cruiser': 3, 'submarine': 4, 'destroyer':5}
+boat2num = {'battleship': 'b', 'carrier': 2, 'cruiser': 3, 'submarine': 4, 'destroyer':5}
 def place_boat(boat, bow, direction, board, boat2length, boat2num):
+    letter2num= {'a' : 0, 'b': 1, 'c' : 2, 'd': 3}
     """
     boat: which boat is it?
     bow: where is the front of the boat?
@@ -16,51 +17,87 @@ def place_boat(boat, bow, direction, board, boat2length, boat2num):
     boat2length: dictionary tells how long each boat is
     boat2num: dictionary numbers each boat
     """
+    #TODO: incorporate letter2num
+    #TODO: change so that Jarvis is picking in a more intelligent way
+    bow = parse_coordinate(bow)
     board[bow[0]][bow[1]] = boat2num[boat]
     go = True
     try:
-        if direction == 'north':
+        #make it so you can't wrap around
+        if direction == 'n':
             for i in range(1, boat2length[boat]):
-                board[bow[0]][bow[1]] = boat2num[boat]
-        elif direction == 'south':
+                if(board[bow[0]+i][bow[1]]) != 0:
+                    raise IndexError
+                else:
+                    board[bow[0]+i][bow[1]] = boat2num[boat]
+        elif direction == 's':
             for i in range(1, boat2length[boat]):
-                board[bow[0]][bow[1]] = boat2num[boat] 
-        elif direction == 'east':
+                if(board[bow[0]-i][bow[1]]) != 0:
+                    raise IndexError
+                else:
+                    board[bow[0]-i][bow[1]] = boat2num[boat] 
+        elif direction == 'e':
             for i in range(1, boat2length[boat]):
-                board[bow[0]-i][bow[1]] = boat2num[boat] 
+                if(board[bow[0]][bow[1]+i]) != 0:
+                    raise IndexError
+                else:
+                    board[bow[0]][bow[1]+i] = boat2num[boat] 
+                    
+        #default direction is West
         else:
             for i in range(1, boat2length[boat]):
-                board[bow[0]+i][bow[1]] = boat2num[boat] 
-    except: #except statement for if the boat is going off the grid
-        print('You cannot place your boat there. Try again')
+                if(board[bow[0]][bow[1]-i]) != 0:
+                    raise IndexError
+                else:
+                    board[bow[0]][bow[1]-i] = boat2num[boat] 
+    except IndexError: #except statement for if the boat is going off the grid
         go = False
     #raise an error
     return board, go
 
 def load_gameboard():
-    line_a = [0,0,0,0]
-    line_b = [0,0,0,0]
-    line_c = [0,0,0,0]
-    line_d = [0,0,0,0]
-    gameboard = {a : line_a,b : line_b,c : line_c, d : line_d}
+    gameboard = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+#    gameboard = {'a' : line_a,'b' : line_b,'c' : line_c, 'd' : line_d}
     return gameboard
 
-def fire(location,gameboard_opponant):
+def fire(location, guess_board,gameboard_opponant):
+    location = parse_coordinate(location)
     if gameboard_opponant[location[0]][location[1]] == -1:
-        print("Already fired there, please choose new coordinates.")
+        return("Already fired there, please choose new coordinates.", guess_board, gameboard_opponant)
     elif gameboard_opponant[location[0]][location[1]] == 0:
         gameboard_opponant[location[0]][location[1]] = -1
-        print("Missed")
+        if guess_board:
+            guess_board[location[0]][location[1]] = 'O'
+        return("Missed", guess_board,gameboard_opponant)
     else:
-        print("Hit")
+        #im not sure if this is going to work?
         count = 0
         for line in gameboard_opponant:
             for coord in line:
                 if coord == gameboard_opponant[location[0]][location[1]]:
                     count += 1
             if count == 0:
-                print("You have sunk the opponant's ship!")
+                if guess_board:
+                    guess_board[location[0]][location[1]] = 'X'
+                return("You have sunk the opponent's ship!", guess_board,gameboard_opponant)
         gameboard_opponant[location[0]][location[1]] = -1
+        if guess_board:
+            guess_board[location[0]][location[1]] = 'X'
+        return("Hit!", guess_board,gameboard_opponant)
+
+def start_game():
+    b1 = load_gameboard()
+    b2 = load_gameboard()
+    return b1, b2
+    
+    
+def parse_coordinate(coord):
+    letter2num= {'a' : 0, 'b': 1, 'c' : 2, 'd': 3}    
+    coord = [x for x in coord]
+    coord[0] = letter2num[coord[0]]
+    coord[1] = int(coord[1]) - 1
+    return coord
+
 
 
     
